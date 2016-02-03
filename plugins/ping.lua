91b24610358505a711732a4f824ebf55aeef06cc
Zihao Wang <dev@wzhd.org>
2016-02-03 20:09:33 +0800

    Send 'Pong!' as reply.

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -1,14 +1,17 @@
  -- Actually the simplest plugin ever!
 
+local command = 'ping'
+
 local triggers = {
 	'^/ping[@'..bot.username..']*'
 }
 
 local action = function(msg)
-	sendMessage(msg.chat.id, 'Pong!')
+        sendMessage(msg.chat.id, 'Pong!', true, msg.message_id, true)
 end
 
 return {
 	action = action,
-	triggers = triggers
+        command = command,
+        triggers = triggers
 }
